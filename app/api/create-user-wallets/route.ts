26aba3d4a2641f299dc090c301b244d4b55ffc2c
Rhyguy <rhyan@readyweb3.io>
2023-11-13 20:42:51 -0800

    adjust api route

--- app/api/create-user-wallets/route.ts
+++ app/api/create-user-wallets/route.ts
@@ -1,25 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-	// TODO: Get webflow user email from request body...
-	// const { email } = await request.json();
-	const options = {
-		method: "POST",
-		headers: {
-			accept: "application/json",
-			"content-type": "application/json",
-			"x-api-key": process.env.METAKEEP_API_KEY,
-		},
-		body: JSON.stringify({ user: { email: "rhyan@readyweb3.io" } }),
-	};
+export async function POST(req: Request) {
+	try {
+		// TODO: Get webflow user email from request body...
+		const { payload } = await req.json();
+		if (payload === undefined) {
+			throw new Error("Payload is undefined!");
+		}
+		const data = payload?.data;
+		const { email } = data;
 
-	// Create user wallet...
-	const res = await fetch(
-		"https://api.metakeep.xyz/v3/getWallet",
-		options
-	).then((res) => res.json());
+		// Create user wallet...
+		const options = {
+			method: "POST",
+			headers: {
+				accept: "application/json",
+				"content-type": "application/json",
+				"x-api-key": process.env.METAKEEP_API_KEY,
+			},
+			body: JSON.stringify({ user: { email } }),
+		};
 
-	console.log(res);
+		const res = await fetch(
+			"https://api.metakeep.xyz/v3/getWallet",
+			options
+		).then((res) => res.json());
 
-	return NextResponse.json(res);
+		return NextResponse.json({
+			message: "User wallet created successfully!",
+			email: email,
+			...res,
+		});
+	} catch (err) {
+		NextResponse.json(
+			{ message: `Error creating user wallet! ${err}` },
+			{ status: 500 }
+		);
+	}
 }
