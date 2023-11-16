f51260625c25d3c899372b97da04617dfc2e0063
Rhyguy <rhyan@readyweb3.io>
2023-11-16 10:36:06 -0800

    remove unused import. Update user in webflow with wallets. update error handling

--- app/api/create-user-wallets/route.ts
+++ app/api/create-user-wallets/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 	try {
-		// TODO: Get webflow user email from request body...
+		// Get User - created in webflow...https://docs.developers.webflow.com/v2.0.0-beta/reference/all-events#user_account_added
 		const { payload } = await req.json();
 		if (payload === undefined) {
 			throw new Error("Payload is undefined!");
 		}
-		const data = payload?.data;
-		const { email } = data;
+		const { id: userId, isEmailVerified, status: userStatus } = payload;
+		const { email, name } = payload?.data;
 
-		// Create user wallet...
+		// Get or Create user wallet...
 		const options = {
 			method: "POST",
 			headers: {
@@ -26,11 +26,57 @@ export async function POST(req: Request) {
 			options
 		).then((res) => res.json());
 
-		return NextResponse.json({
+		// Did we get Wallets?...
+		switch (res.status) {
+			case "INVALID_EMAIL":
+				throw new Error("Invalid email!");
+			case "INVALID_REQUEST":
+				throw new Error(
+					"The request is not valid e.g. if the request json has unsupported fields."
+				);
+			case "SOMETHING_WENT_WRONG":
+				throw new Error(
+					"An unknown error occurred. Please get in touch with us if you continue seeing this error."
+				);
+		}
+
+		// ...IF success, update Webflow User wallets...
+		const { ethAddress, solAddress, eosAddress } = res?.wallet;
+		const webflowUserWallets = {
+			"eth-address": ethAddress,
+			"sol-address": solAddress,
+			"eos-address": eosAddress,
+		};
+		const updateWebflowUserOptions = {
+			method: "PATCH",
+			headers: {
+				accept: "application/json",
+				"content-type": "application/json",
+			},
+			body: JSON.stringify({
+				data: { ...webflowUserWallets },
+			}),
+		};
+
+		fetch(
+			`https://api.webflow.com/beta/sites/${process.env.WEBFLOW_SITE_ID}/users/${userId}`,
+			updateWebflowUserOptions
+		)
+			.then((response) => response.json())
+			.then((response) => console.log(response))
+			.catch((err) => console.error(err));
+
+		// Return response...
+		const userData = {
 			message: "User wallet created successfully!",
-			email: email,
+			userId,
+			isEmailVerified,
+			email,
+			name,
+			userStatus,
 			...res,
-		});
+		};
+		return NextResponse.json(userData);
 	} catch (err) {
 		NextResponse.json(
 			{ message: `Error creating user wallet! ${err}` },
